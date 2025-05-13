a906fd320583c362a8d3c2d7f9c03d55c9c0405e
Min-byeonghyun <qudgus5125@naver.com>
2025-05-14 00:05:14 +0900

    상태관리

---
+++ src/components/OpenModalButton.jsx
@@ -0,0 +1,17 @@
+import { useDispatch } from "react-redux"
+import { OPEN_MODAL } from "../reducer"
+
+export default function OpenModalButton() {
+
+  const dispatch = useDispatch()
+
+  const openModal = () => {
+    dispatch({type : OPEN_MODAL})
+  }
+
+  return(
+    <button onClick={openModal}>
+      모달 열기
+    </button>
+  )
+}
---
+++ src/reducer/index.js
@@ -0,0 +1,47 @@
+export const TOGGLE_MODAL = "TOGGLE_MODAL"
+export const CLOSE_MODAL = "CLOSE_MODAL"
+export const OPEN_MODAL = "OPEN_MODAL"
+
+const initialState = {
+  modal : {
+    isShow: false,
+
+  }
+}
+
+/**
+ * 
+ * @param {*} state : 현재 상태 
+ * @param {*} action : 액션 객체(함수를 호출하는 주문)
+ * @returns 
+ */
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_MODAL :
+      return{
+        ...state,
+        modal: {
+          isShow : !state.modal.isShow
+        }
+      }
+      case CLOSE_MODAL :
+      return{
+        ...state,
+        modal: {
+          isShow : false
+        }
+      }
+      case OPEN_MODAL :
+      return{
+        ...state,
+        modal: {
+          isShow : true
+        }
+      }
+      default: 
+      return state;
+  }
+}
+
+export default rootReducer
